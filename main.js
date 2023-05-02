89318141073fb90e7209971046dda3e3f0650108
Tuan <tuanstarkpd@gmail.com>
2023-05-02 20:22:41 +0700

    add cmt to main.js file

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'OrbitControls';
 
+
+/////////// lấy các url tới các hình ảnh
 const galaxyTexture = new Image() 
 galaxyTexture.src = 'http://127.0.0.1:5500/../img/galaxy.jpg'
-
+console.log('url:',galaxyTexture.src)
 //import starsTexture from '../img/stars.jpg';
 const starsTexture = new Image() 
 starsTexture.src = 'http://127.0.0.1:5500/../img/stars.jpg'
@@ -57,29 +59,32 @@ const plutoTexture = new Image()
 plutoTexture.src = 'http://127.0.0.1:5500/../img/pluto.jpg'
 
 
+////////////Tạo trình kết xuất, giống như việc chiếu bộ phim lên màn hình tv, dt, màn chiếu
 const renderer = new THREE.WebGLRenderer();
-
+/////////// chọn kích thước khung chiếu lên màn hình
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
-
+///////////Tạo cảnh
 const scene = new THREE.Scene();
-
+///////////thiết lập camera
 const camera = new THREE.PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
 );
-
+////////// thay đổi vị trí camera bằng thao tác chuột
 const orbit = new OrbitControls(camera, renderer.domElement);
-
+/////////thiết lập vị trí camera
 camera.position.set(-90, 140, 140);
-orbit.update();
 
+//////// cập nhật lại cảnh sau khi thay đổi vị trí
+orbit.update();
+/////// thiết lập ánh sáng xunh quanh
 const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
-
+/////// tạo nền bằng ảnh bầu trời sao cho sáu mặt
 const cubeTextureLoader = new THREE.CubeTextureLoader();
 scene.background = cubeTextureLoader.load([
     starsTexture.src,
@@ -89,7 +94,7 @@ scene.background = cubeTextureLoader.load([
     starsTexture.src,
     starsTexture.src
 ]);
-
+////// tạo mặt trời
 const textureLoader = new THREE.TextureLoader();
 
 const sunGeo = new THREE.SphereGeometry(16, 30, 30);
@@ -98,16 +103,20 @@ const sunMat = new THREE.MeshBasicMaterial({
 });
 const sun = new THREE.Mesh(sunGeo, sunMat);
 scene.add(sun);
-
+////// ý tưởng tạo hệ mặt trời: tạo mặt trời nằm ở giữa các hành tinh quay quanh mặt trời với tốc độ khác nhau, các hành tinh cx tự quay quanh mình với tốc độ khác nhau
+////// cách làm chi tiết:
+//////hàm tạo các hành tinh
 function createPlanete(size, texture, position, ring) {
-    const geo = new THREE.SphereGeometry(size, 30, 30);
+    const geo = new THREE.SphereGeometry(size, 30, 30);// 2 tham số sau là số kinh tuyến của mặt cầu và số mặt phẳng tạo mặt cầu giữa 2 kinh tuyến
     const mat = new THREE.MeshStandardMaterial({
         map: textureLoader.load(texture)
     });
     const mesh = new THREE.Mesh(geo, mat);
     const obj = new THREE.Object3D();
-    obj.add(mesh);
-    if(ring) {
+    obj.add(mesh);// nếu đoạn này add obj vào sun thì các hành tinh đều có tốc độ quay quanh sun giống nhau(cùng tốc độ quay quanh trục với mặt trời)
+    //, vì v với mỗi hành tinh ta sẽ tạo obj3D nằm tại vị trí mặt trời, r add hành tinh đó vô obj3D
+    //muốn các hành tinh quay với tốc độ nào thì set obj quay với tốc độ đó
+    if(ring) {//ring!=Null
         const ringGeo = new THREE.RingGeometry(
             ring.innerRadius,
             ring.outerRadius,
@@ -122,10 +131,10 @@ function createPlanete(size, texture, position, ring) {
         ringMesh.rotation.x = -0.5 * Math.PI;
     }
     scene.add(obj);
-    mesh.position.x = position;
-    return {mesh, obj}
+    mesh.position.x = position;// các hành tinh ban đầu đều nằm trên 1 đường thẳng
+    return {mesh, obj}// trả về 1 đối tượng 2 thuộc tính : hành tinh và cái điểm quay của nó
 }
-
+//////////tạo các hành tinh
 const mercury = createPlanete(3.2, mercuryTexture.src, 28);
 const venus = createPlanete(5.8, venusTexture.src, 44);
 const earth = createPlanete(6, earthTexture.src, 62);
@@ -146,9 +155,9 @@ const pluto = createPlanete(2.8, plutoTexture.src, 216);
 
 const pointLight = new THREE.PointLight(0xFFFFFF, 2, 300);
 scene.add(pointLight);
-
+///////// hàm thực hiện các phép biến đổi
 function animate() {
-    //Self-rotation
+    //Self-rotation, tự quay quanh trục
     sun.rotateY(0.004);
     mercury.mesh.rotateY(0.004);
     venus.mesh.rotateY(0.002);
